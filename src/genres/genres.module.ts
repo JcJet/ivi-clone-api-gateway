b92041ac58731950510620d70b2bbe7794a82bf4
Sergey Klimov <myxxze@gmail.com>
2023-04-29 21:53:01 +0300

    Renamed Genres queue in RMQ.

--- src/genres/genres.module.ts
+++ src/genres/genres.module.ts
@@ -11,7 +11,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.RMQ,
         options: {
           urls: ['amqp://localhost:5672'],
-          queue: 'to_genres_ms',
+          queue: 'toGenresMs',
           queueOptions: {
             durable: false,
           },
