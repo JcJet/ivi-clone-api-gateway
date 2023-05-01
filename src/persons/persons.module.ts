b3ca1db43aac72c60c2df33995d131605ca56410
Sergey Klimov <myxxze@gmail.com>
2023-05-01 13:16:04 +0300

    Added transport to Persons Module.

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PersonsController } from './persons.controller';
 import { PersonsService } from './persons.service';
+import {ClientsModule, Transport} from "@nestjs/microservices";
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ToPersonsMs',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'toPersonsMs',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [PersonsController],
   providers: [PersonsService],
 })
