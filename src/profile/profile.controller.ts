4e2cb87916895431c7c2925d2fb2351da0d57db7
Sergey Klimov <myxxze@gmail.com>
2023-05-04 13:48:43 +0300

    Removed Authorization Module - all endpoints moved to Profile Module as one entry point to all auth & profile actions.

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { RegistrationDto } from './dto/registration.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+  constructor(private profileService: ProfileService) {}
+
+  @Post()
+  registration(@Body() registrationDto: RegistrationDto) {
+    console.log(
+      'API Gateway - Profile Controller - registration at',
+      new Date(),
+    );
+    return this.profileService.registration(registrationDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+    console.log(
+      'API Gateway - Profile Controller - login at',
+      new Date(),
+    );
+    return this.profileService.login(loginDto);
+  }
+}
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { RegistrationDto } from './dto/registration.dto';
+import { LoginDto } from './dto/login.dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(@Inject('ToProfileMs') private profileProxy: ClientProxy) {}
+
+  async registration(registrationDto: RegistrationDto) {
+    console.log('API Gateway - Profile Service - registration at', new Date());
+    return this.profileProxy.send(
+      { cmd: 'registration' },
+      { ...registrationDto },
+    );
+  }
+
+  async login(loginDto: LoginDto) {
+    console.log('API Gateway - Profile Service - login at', new Date());
+    return this.profileProxy.send({ cmd: 'login' }, { ...loginDto });
+  }
+}
