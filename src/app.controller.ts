df5a4695c730e1e93c1b36d23301b12c084d16c9
Sergey <myxxze@gmail.com>
2023-05-08 00:52:25 +0300

    Api updates.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Main application (SERVICE USE ONLY)')
 export class AppController {
   constructor(private appService: AppService) {}
 
   @Get('/loadDatabase')
+  @ApiOperation({ summary: 'Load all movie data into DBs.' })
   loadDatabase() {
     return this.appService.loadDatabase();
   }
