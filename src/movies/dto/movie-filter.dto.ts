eb3dca4e8eadae5825f1d727ed9fee5409cd37e2
Sergey Klimov <myxxze@gmail.com>
2023-05-10 17:59:41 +0300

    movie-filter.dto.ts commented out.

--- src/movies/dto/movie-filter.dto.ts
+++ src/movies/dto/movie-filter.dto.ts
@@ -1,24 +1,24 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class MovieFilterDto {
-  @ApiProperty()
-  readonly genre: string;
-
-  @ApiProperty()
-  readonly country: string;
-
-  @ApiProperty()
-  readonly year: number;
-
-  @ApiProperty()
-  readonly rating: number;
-
-  @ApiProperty()
-  readonly ratingCount: number;
-
-  @ApiProperty()
-  readonly director: string;
-
-  @ApiProperty()
-  readonly actor: string;
-}
+// import { ApiProperty } from '@nestjs/swagger';
+//
+// export class MovieFilterDto {
+//   @ApiProperty()
+//   readonly genre: string;
+//
+//   @ApiProperty()
+//   readonly country: string;
+//
+//   @ApiProperty()
+//   readonly year: number;
+//
+//   @ApiProperty()
+//   readonly rating: number;
+//
+//   @ApiProperty()
+//   readonly ratingCount: number;
+//
+//   @ApiProperty()
+//   readonly director: string;
+//
+//   @ApiProperty()
+//   readonly actor: string;
+// }
