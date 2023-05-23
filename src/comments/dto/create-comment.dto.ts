2b3ad58e927c838b63f49d778378fb71774f8796
JcJet <jcjet88@gmail.com>
2023-05-23 16:44:37 +0300

    uptaded create-comment.dto.ts

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,8 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCommentDto {
-  @ApiProperty({ example: '1', description: 'Идентификатор автора' })
-  readonly userId: number;
+  @ApiProperty({
+    example: `{"userId": 1, "name": "Nebuchadnezzar"}`,
+    description: 'Автор комментария',
+  })
+  readonly author: {
+    readonly userId: number;
+    readonly name: string;
+  };
 
   @ApiProperty({ example: 'любой текст', description: 'Текст комментария' })
   readonly text: string;
@@ -23,5 +29,6 @@ export class CreateCommentDto {
     example: '2019-04-23T18:25:43.511Z',
     description: 'Дата создания комментария',
   })
-  readonly creationDate: Date;
+  readonly date: Date;
 }
+
