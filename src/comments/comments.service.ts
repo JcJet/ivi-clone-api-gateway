9c47d161a7868fd9d336be862170901f9a54012a
JcJet <jcjet88@gmail.com>
2023-05-25 12:46:33 +0300

    changed querry params

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { GetCommentsDto } from './dto/get-comments.dto';
@@ -7,12 +7,45 @@ import { GetCommentsDto } from './dto/get-comments.dto';
 export class CommentsService {
   constructor(@Inject('ToCommentsMs') private commentsProxy: ClientProxy) {}
 
-  async createComment(dto: CreateCommentDto) {
+  createEssenceDto(movieId, commentId) {
+    let essenceTable: string;
+    let essenceId: number;
+    if (movieId) {
+      essenceTable = 'movies';
+      essenceId = movieId;
+    } else if (commentId) {
+      essenceTable = 'comments';
+      essenceId = commentId;
+    }
+    return { essenceTable, essenceId };
+  }
+  checkInputs(movieId, commentId) {
+    if (movieId && commentId) {
+      throw new HttpException(
+        'Противоречивый запрос: указаны movieId и commentId',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    if (!(movieId || commentId)) {
+      throw new HttpException(
+        'Не задан movieId или commentId (чувствительно к регистру)',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+  async createComment(
+    createCommentDto: CreateCommentDto,
+    movieId: number,
+    commentId: number,
+  ) {
     console.log(
       'API Gateway - Comments Service - createComment at',
       new Date(),
     );
-    return this.commentsProxy.send({ cmd: 'createComment' }, { dto });
+    this.checkInputs(movieId, commentId);
+    const essenceDto = this.createEssenceDto(movieId, commentId);
+    const fullDto = { ...createCommentDto, ...essenceDto };
+    return this.commentsProxy.send({ cmd: 'createComment' }, { dto: fullDto });
   }
 
   async deleteComment(commentId: number) {
@@ -37,11 +70,14 @@ export class CommentsService {
     console.log('API Gateway - Comments Service - getComments at', new Date());
     return this.commentsProxy.send({ cmd: 'getComments' }, { dto });
   }
-  async getCommentsTree(dto: GetCommentsDto) {
+  async getCommentsTree(movieId: number, commentId: number) {
     console.log(
       'API Gateway - Comments Service - getCommentsTree at',
       new Date(),
     );
+
+    this.checkInputs(movieId, commentId);
+    const dto = this.createEssenceDto(movieId, commentId);
     return this.commentsProxy.send({ cmd: 'getCommentsTree' }, { dto });
   }
 
