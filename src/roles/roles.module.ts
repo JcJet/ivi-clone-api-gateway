f57a8129fe8d05b3adbba5595cd24874bcc4493c
Sergey Klimov <myxxze@gmail.com>
2023-06-01 02:21:38 +0300

    Added transport to roles.module.ts.

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'ToRolesMs',
+        useFactory: () => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [process.env.RMQ_URL],
+            queue: 'toRolesMs',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
   controllers: [RolesController],
-  providers: [RolesService]
+  providers: [RolesService],
 })
 export class RolesModule {}
