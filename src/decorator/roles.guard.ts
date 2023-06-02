d1767bb041908ed4d722d5a6e725d08044e1bf1c
Sergey Klimov <myxxze@gmail.com>
2023-06-03 02:54:19 +0300

    Added roles.guard.ts.

---
+++ src/decorator/roles.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const req = context.switchToHttp().getRequest();
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
+    const user = this.jwtService.verify(token);
+    const userRoles = user.roles;
+    req.user = user;
+    console.log(user);
+    const access = userRoles.some((role) => requiredRoles.includes(role));
+    if (access) {
+      return true;
+    }
+    throw new HttpException('Нет доступа', HttpStatus.FORBIDDEN);
+  }
+}
